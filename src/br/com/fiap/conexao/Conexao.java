3a36e69edb278530f17fef93a2a5f579611e191b
Guilherme Bigois <guilhermebigois@outlook.com>
2017-09-10 00:32:48 -0300

    Adição dos atributos da conexão

--- src/br/com/fiap/conexao/Conexao.java
+++ src/br/com/fiap/conexao/Conexao.java
@@ -1,5 +1,15 @@
 package br.com.fiap.conexao;
 
-public class Conexao {
+import com.mysql.jdbc.Connection;
 
+@SuppressWarnings("unused")
+public class Conexao {
+	private static Connection connection;
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String SERVIDOR = "127.0.0.1";
+	private static final String PORTA = "3306";
+	private static final String BANCO = "FIAP";
+	private static final String URL = "jdbc:mysql:thin:@";
+	private static final String USUARIO = "root";
+	private static final String SENHA = "";
 }
